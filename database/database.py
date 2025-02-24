dd21af48ea2725f8a0cd535d3d48e87bb4c66564
nikita <vnd-08@yandex.ru>
2025-02-24 16:46:12 +0300

    Changed files' structure not to cause a bug with running as a package. Implemented registration.

--- database/database.py
+++ database/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_PATH = "sqlite:///data.db"
+DB_PATH = "sqlite:///hundred-to-one/data.db"
 engine = create_engine(DB_PATH)
 Session = sessionmaker(autoflush=False, bind=engine)
---
+++ website/main.py
@@ -0,0 +1,57 @@
+import uuid
+
+from flask import Flask, render_template, request, redirect
+from werkzeug.security import generate_password_hash
+
+from ..database import models, database
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "1"
+
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+def register_user():
+    form = request.form
+    session = database.Session()
+
+    email = form.get("email")
+    username = form.get("username")
+    password = form.get("password")
+    existing_user = session.query(models.User).filter(models.User.email == email).one_or_none()
+    if existing_user:
+        return f"User with {email} already exists.", 409
+
+    new_user = models.User(id=str(uuid.uuid4()), username=username, email=email,
+                           passwd_hash=generate_password_hash(password))
+    session.add(new_user)
+    session.commit()
+    session.close()
+
+    return redirect("index")
+
+
+@app.route("/auth", methods=["POST", "GET"])
+def auth():
+    if request.method == "POST":
+        action = request.form.get("action-type")
+        if action == "reg":
+            return register_user()
+        elif action == "login":
+            raise NotImplementedError
+        else:
+            return "Unknown action", 400
+
+    return render_template("auth.html")
+
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    models.Base.metadata.create_all(database.engine)
+
+    app.run(port=8000, host="0.0.0.0", use_reloader=True)

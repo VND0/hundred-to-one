35ee5b04b71cb45b397d0550d87cf5bbe6b4eb7f
nikita <vnd-08@yandex.ru>
2025-03-27 13:07:38 +0300

    Removed todos. Added cascade deletion of empty polls and related answers on the deletion of questions

--- questions_resource.py
+++ questions_resource.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 
 import models
 from database.database import session_generator
-from database.db_models import Question
+from database.db_models import Question, Answer, Poll
 from tools import what_happened
 
 sessions = session_generator()
@@ -46,12 +46,22 @@ class QuestionResource(Resource):
 
         session = next(sessions)
         question = session.query(Question).filter(Question.id == question_id).one()
-        # TODO: implement cascade deletion of polls and answers
+
+        # Removing answers
+        session.query(Answer).filter(Answer.question_id == question_id).delete()
+
         try:
             session.delete(question)
             session.commit()
         except IntegrityError:
             abort(409, message=InterruptedError.__name__)
+
+        # Removing empty polls
+        polls = session.query(Poll).all()
+        for poll in polls:
+            if not poll.questions:
+                session.delete(poll)
+        session.commit()
 
         return jsonify(204, {"success": "OK"})
 

d5d1bcdbc0edfa5aba5b6d2bf73ce28870531632
nikita <vnd-08@yandex.ru>
2025-04-16 19:14:22 +0300

    Added endpoint to get game by its id

--- resources/games_resource.py
+++ resources/games_resource.py
@@ -17,3 +17,12 @@ class GamesResource(Resource):
         db.session.delete(game)
         db.session.commit()
         return jsonify("OK")
+
+    @jwt_required()
+    def get(self, game_id: str):
+        jwt_user_id = get_jwt_identity()
+        game = db.session.query(Game).filter(Game.id == game_id and Game.user_id == jwt_user_id).one_or_none()
+        if game is None:
+            abort(404, message=f"Game with ID {game_id} not found.")
+
+        return game.to_dict(rules=("-user", "-questions.user", "-questions.answers.question"))

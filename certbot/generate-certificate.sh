d409386776c79386d0f9ad05dc3e6a495e61db34
nikita <vnd-08@yandex.ru>
2025-05-13 22:58:46 +0300

    Changed certbot script

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -5,8 +5,8 @@ rm -rf /etc/letsencrypt/live/certfolder*
 certbot certonly --standalone --email "$DOMAIN_EMAIL" -d "$DOMAIN_URL" \
   --cert-name=certfolder --key-type rsa --agree-tos
 
-rm -f /etc/nginx/cert.pem
-rm -f /etc/nginx/key.pem
+rm -rf /etc/nginx/cert.pem
+rm -rf /etc/nginx/key.pem
 
 cp /etc/letsencrypt/live/certfolder*/fullchain.pem /etc/nginx/cert.pem
 cp /etc/letsencrypt/live/certfolder*/privkey.pem /etc/nginx/key.pem
